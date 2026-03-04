9d1e04455628a3a88dd8005d757c8647a21c4028
TOliverK <kocsis.tamas.oliver@gmail.com>
2026-03-04 20:15:29 +0000

    Add fifo buffer alloc / dealloc

--- testfifo.c
+++ testfifo.c
@@ -5,6 +5,7 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Oliver K");
@@ -15,6 +16,13 @@ static dev_t dev_num;
 static struct cdev testfifo_cdev;
 static struct class *testfifo_class;
 static struct device *testfifo_device;
+
+#define FIFO_BUF_SIZE 4096  // needs to be pw of 2
+
+static char *fifo_buf;
+static size_t fifo_head;  // next byte to read
+static size_t fifo_tail;  // next byte to write
+static size_t fifo_count;  // bytes currently in buffer
 
 // called when userspace opens /dev/testfifo
 static int testfifo_open(struct inode *inode, struct file *file)
@@ -50,11 +58,22 @@ static int __init testfifo_init(void)
 	}
 	pr_info("testfifo: allocated %d:%d\n", MAJOR(dev_num), MINOR(dev_num));
 
+	// allocate fixed arrray with kmalloc: pinned in RAM, non-swappable
+	// note: kmalloc flags
+	fifo_buf = kmalloc(FIFO_BUF_SIZE, GFP_KERNEL);
+	if (!fifo_buf) {
+		pr_err("testfifo: failed to allocate buffer\n");
+		unregister_chrdev_region(dev_num, 1);
+		return -ENOMEM;
+	}
+	pr_info("testfifo: buffer allocated (%d bytes)\n", FIFO_BUF_SIZE);
+
 	// wire file_operations into the cdev, then register it with our device number
 	cdev_init(&testfifo_cdev, &testfifo_fops);
 	ret = cdev_add(&testfifo_cdev, dev_num, 1);
 	if (ret < 0) {
 		pr_err("testfifo: failed to add cdev\n");
+		kfree(fifo_buf);
 		unregister_chrdev_region(dev_num, 1);
 		return ret;
 	}
@@ -65,6 +84,7 @@ static int __init testfifo_init(void)
 	testfifo_class = class_create("testfifo");
 	if (IS_ERR(testfifo_class)) {
 		pr_err("testfifo: failed to create class\n");
+		kfree(fifo_buf);
 		cdev_del(&testfifo_cdev);
 		unregister_chrdev_region(dev_num, 1);
 		return PTR_ERR(testfifo_class);
@@ -74,6 +94,7 @@ static int __init testfifo_init(void)
 	testfifo_device = device_create(testfifo_class, NULL, dev_num, NULL, "testfifo");
 	if (IS_ERR(testfifo_device)) {
 		pr_err("testfifo: failed to create device\n");
+		kfree(fifo_buf);
 		class_destroy(testfifo_class);
 		cdev_del(&testfifo_cdev);
 		unregister_chrdev_region(dev_num, 1);
@@ -90,6 +111,7 @@ static void __exit testfifo_exit(void)
 	// .owner ref count>0 forbids calling rmmod and this exit
 	device_destroy(testfifo_class, dev_num);
 	class_destroy(testfifo_class);
+	kfree(fifo_buf);
 	cdev_del(&testfifo_cdev);
 	unregister_chrdev_region(dev_num, 1);
 	pr_info("testfifo: unloaded\n");
